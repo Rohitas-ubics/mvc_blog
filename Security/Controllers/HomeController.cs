07d6b6b3e6d5faad822482f0a2fcb90928de8d0f
rohitas <rohitas.ubics@gmail.com>
2015-12-30 14:15:33 +0530

    second

---
+++ Security/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Security.DAL.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Security.Controllers
+{
+    
+    public class HomeController : BaseController
+    {
+        //
+        // GET: /constructor/             
+        public ActionResult Index()
+        {
+            string FullName = User.FirstName + " " + User.LastName;
+            return View();
+        }
+
+        [CustomAuthorize(Roles = "User")]
+        public ActionResult Write_post()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
